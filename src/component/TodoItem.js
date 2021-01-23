86954fb73b618ddc3ecc4efdd1fd4634a122d091
nghicv <nghicv@beetinnovators.com>
2021-01-23 20:47:25 +0700

    init project

---
+++ src/component/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component} from 'react';
+
+class TodoItem extends component {
+    render() {
+        return(
+            <div>Đi chợ</div>
+        );
+    }
+}
+
+export default TodoItem;
